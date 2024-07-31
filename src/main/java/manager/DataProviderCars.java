fd8c605a392ec099d34d97175995ca35ee8b57ec
YELENA-ZALMAYEV <elenkina73@gmail.com>
2024-07-31 17:33:31 +0300

    home work provider

--- src/main/java/manager/DataProviderCars.java
+++ src/main/java/manager/DataProviderCars.java
@@ -6,12 +6,14 @@ import org.testng.annotations.DataProvider;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class DataProviderCars {
 
     @DataProvider
     public Iterator<Object []> addCarSuccess(){
         List<Object []> list = new ArrayList<>();
+        int i = new Random().nextInt(100)+1000;
         list.add(new Object[]{Car.builder()
                 .location("Tel Aviv, Israel")
                 .manufacture("Opel")
@@ -20,7 +22,7 @@ public class DataProviderCars {
                 .fuel("Petrol")
                 .seats(5)
                 .carClass("D")
-                .carRegNumber("252-2525")
+                .carRegNumber("252-2525"+i)
                 .price(12)
                 .about("good")
                 .build()
@@ -34,7 +36,7 @@ public class DataProviderCars {
                 .fuel("Petrol")
                 .seats(5)
                 .carClass("D")
-                .carRegNumber("252-2125")
+                .carRegNumber("252-2125"+i)
                 .price(12)
                 .about("good")
                 .build()
@@ -48,7 +50,7 @@ public class DataProviderCars {
                 .fuel("Petrol")
                 .seats(5)
                 .carClass("D")
-                .carRegNumber("252-2335")
+                .carRegNumber("252-2335"+i)
                 .price(12)
                 .about("good")
                 .build()
